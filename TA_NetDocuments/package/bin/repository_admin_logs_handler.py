1f24b0c20d547c1d6c7187fcda49a4ecba43856a
Vatsal Jagani <vatsaljagani85@gmail.com>
2024-02-17 22:52:34 +0530

    Missing python file added.

---
+++ TA_NetDocuments/package/bin/repository_admin_logs_handler.py
@@ -0,0 +1,11 @@
+
+from splunklib import modularinput as smi
+
+
+def validate_input(input_script: smi.Script, definition: smi.ValidationDefinition):
+    return
+
+
+def stream_events(input_script: smi.Script, inputs: smi.InputDefinition, event_writer: smi.EventWriter):
+    return
+
